cd477e1f99e4d633f6712509fde18f675570d773
Yassine-MAKHLOUK <yassinemakhlouk0@gmail.com>
2024-05-22 00:53:15 +0100

    consume login api

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -6,6 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog'
 import { MatIconModule } from '@angular/material/icon';
+import { AuthServiceService } from '../../services/auth-service.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 export interface LoginDialogData {
   email: string;
@@ -29,12 +32,18 @@ export interface LoginDialogData {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   
-  constructor(
+  constructor(private authService : AuthServiceService,
+    private router: Router,
     public dialogRef: MatDialogRef<LoginComponent>,
     @Inject(MAT_DIALOG_DATA) public data: LoginDialogData,
   ) {}
+  ngOnInit(): void {
+  }
+
+  private subscription: Subscription = new Subscription;
+
 
   public showPassword: boolean = false;
   private fb = inject(FormBuilder);
@@ -48,8 +57,24 @@ export class LoginComponent {
 
   hasUnitNumber = false;
 
+  login(credentials: any) {
+    this.authService.login(credentials).subscribe(
+      response => {
+        console.log('Login successful', response);
+        localStorage.setItem("token", response.data.token)
+        // Handle successful login, e.g., store the token, navigate to another page
+        this.router.navigate(['/barbers']); // Redirect to the "book" route
+
+      },
+      error => {
+        console.error('Login failed', error);
+        // Handle login failure, e.g., show an error message
+      }
+    );
+  }
+
   onSubmit(){
-    console.log(this.LoginForm.value);
+    this.login(this.LoginForm.value);
     this.dialogRef.close();
   }
   onNoClick(): void {
@@ -57,5 +82,11 @@ export class LoginComponent {
   }
   public togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
