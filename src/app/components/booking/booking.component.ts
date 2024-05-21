133ec9a29b7242da4aab9d48ef0df41fe0c6a6c7
Yassine-MAKHLOUK <yassinemakhlouk0@gmail.com>
2024-05-21 09:19:34 +0100

    add reservation form(without style)

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FloatLabelType, MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './booking.component.html',
+  providers:[provideNativeDateAdapter()],
   styleUrl: './booking.component.css'
 })
 export class BookingComponent {
+  private fb = inject(FormBuilder);
+  addressForm = this.fb.group({
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    email: [null, Validators.required],
+    phone: null,
+    date:null,
 
+  });
+
+  hasUnitNumber = false;
+
+  onSubmit(){
+    console.log(this.addressForm.value);
+    
+  }
 }
